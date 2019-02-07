8928e3f0ed1037d4f637eca514451c8c2bcd9046
bryanboconzaca <bboconzaca2022@bklawtech.com>
2019-02-07 23:31:58 +0000

    started new project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["space bound","follow you","zero","happier"];
+var images_links = ["https://pbs.twimg.com/profile_images/929030268043845633/ilS1ri2v_400x400.jpg
+ ","
+https://cdn.dribbble.com/users/80333/screenshots/4034543/3.jpg
+","
+https://i.ytimg.com/vi/Zx5670LWOWI/maxresdefault.jpg
+","
+https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/Marshmello_and_Bastille_Happier.png/220px-Marshmello_and_Bastille_Happier.png
+"];
+var artists = ["","","",""];
+var song_lenghts = ["","","",""];
+var links = ["","","",""];
+
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
